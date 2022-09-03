a04395aae71e13af63077b0b1196b16ba4f639eb
SamitMahadi <samithossainmahadi@gmail.com>
2022-09-03 13:57:40 -0400

    added

--- js/app.js
+++ js/app.js
@@ -44,47 +44,50 @@ const ShowNewsItem = (Items) => {
     Items.forEach((item) => {
         const div = document.createElement("div");
         div.innerHTML = `
-        <div class="card mb-3 ">
-        <div class="d-flex justify-content-center">
-          <img class="img-fluid rounded-start"  src="${item.thumbnail_url
-            }" class="card-img-top" alt="...">
-        </div>
-        <div class="card-body">
-          <h5 class="card-title">${item.title}</h5>
-          <p class="card-text">${item.details.substring(0, 200)}...</p>
-          <div class="d-md-flex text-center align-items-center justify-content-between">
-              <div class="d-flex align-items-center">
-                <div class="me-2">
-                  <img class="rounded-circle" style="height: 50px; width: 50px;" src="${item.author.img
+        <div class="card mb-3" style="max-width:fit-content;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${item.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${item.title}</h5>
+                            <p class="card-text">${item.details.substring(0, 200)}...</p>
+
+
+                                <div class="d-md-flex text-center align-items-center justify-content-between">
+                <div class="d-flex align-items-center">
+                    <div class="me-2">
+                        <img class="rounded-circle" style="height: 50px; width: 50px;" src="${item.author.img
             }" alt="">
-                </div>
-                <div>
-                  <p>${item.author.name === null || item.author.name === ""
-                ? ` not found`
+                    </div>
+                    <div>
+                        <p>${item.author.name === null || item.author.name === ""
+                ? `Information not found`
                 : item.author.name
             }</p>
-                  <p>${item.author.published_date}</p>
+                        <p>${item.author.published_date}</p>
+                    </div>
                 </div>
-              </div>
-              <div><i class="fa-regular fa-eye me-2"></i>${item.total_view === null ? `Information not found` : item.total_view
+                <div><i class="fa-regular fa-eye me-2"></i>${item.total_view === null ? ` Not found` : item.total_view
             }</div>
-              <div>
-                <i class="fa-regular fa-star-half-stroke"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-                <i class="fa-regular fa-star"></i>
-              </div>
-              <div>
-              
+                <div>
+                    <i class="fa-regular fa-star-half-stroke"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
                 <button onclick= "showDetail('${item._id
-            }')" class="btn btn-primary" data-bs-toggle="modal"
-                data-bs-target="#newsModal">See More</button>
-                
-              </div>
+            }')" class="btn btn-info text-white" data-bs-toggle="modal"
+                  data-bs-target="#newsModal">See More</button>    
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-      </div>
+
+            
+
         `;
         newsItems.appendChild(div);
     });
