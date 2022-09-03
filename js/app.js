992dc72127a17ec24afd8c19340e98c0c03c5c73
SamitMahadi <samithossainmahadi@gmail.com>
2022-09-03 11:39:05 -0400

    dynamic api  categoies added to website

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
+
+////////////////////Add Categories to the Website////////////
+
 const loadCatagories = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`
     const res = await fetch(url)
     const data = await res.json();
     displayCategories(data.data.news_category);
 }
+loadCatagories()
+
+////show categories and add onclick/////
 const displayCategories = (Categories) => {
     const categoriesContainers = document.getElementById('categories-container')
     Categories.forEach(Category => {
         const CategoryDiv = document.createElement('nav')
         CategoryDiv.classList.add('categories')
         CategoryDiv.innerHTML = `
-        <a class="navbar-brand" href="#">${Category.category_name}</a>
+        <a onclick="ShowNews('${Category.category_id}')" class="navbar-brand" href="#">${Category.category_name}</a>
         `;
         categoriesContainers.appendChild(CategoryDiv);
     });
 }
 
-loadCatagories()
+// //////////load dynamic api for category///////
+const ShowNews = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/{category_id}`
+    const res = await fetch(url)
+    const data = await res.json();
+    console.log(data.data);
+}
+
+
