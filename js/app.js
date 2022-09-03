93c8200568bbf843767f8e208eeaccad972b08b6
SamitMahadi <samithossainmahadi@gmail.com>
2022-09-03 12:08:48 -0400

    show news

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const displayCategories = (Categories) => {
         const CategoryDiv = document.createElement('nav')
         CategoryDiv.classList.add('categories')
         CategoryDiv.innerHTML = `
-        <a onclick="ShowNews('${Category.category_id}')" class="navbar-brand" href="#">${Category.category_name}</a>
+        <a onclick="ShowNews('${Category.category_id}')" class="navbar-brand text-info" href="#">${Category.category_name}</a>
         `;
         categoriesContainers.appendChild(CategoryDiv);
     });
@@ -24,10 +24,22 @@ const displayCategories = (Categories) => {
 
 // //////////load dynamic api for category///////
 const ShowNews = async (id) => {
-    const url = `https://openapi.programming-hero.com/api/news/category/{category_id}`
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
     const res = await fetch(url)
     const data = await res.json();
     console.log(data.data);
 }
+
+
+///////Show Newss////////////
+
+const ShowNewsItem = (Items) => {
+    const itemFound = document.getElementById('item-found')
+    const Found = Items.length;
+    itemFound.innerText = `${Found <= 0 ? "NO DATA FOUND" : Found + "Items FOund"}`
+}
+
+
+
 
 
